414dd3c3a38d77a6be3c04c51ae0bab82a51445c
Dawit-y <dawityimer52@gmail.com>
2024-04-30 22:51:48 +0300

    Add zod validation schema for filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
 import JobListItem from "@/components/jobListItem";
 import JobFilterSideBar from "@/components/JobFilterSideBar";
+import H1 from "@/components/ui/h1";
 
 export default async function Home() {
   const jobs = await prisma.job.findMany({
@@ -10,11 +11,9 @@ export default async function Home() {
 
   return (
     <>
-      <main className="max-w-xl m-auto my-10 space-y-10 px-3">
+      <main className="max-w-2xl m-auto my-10 space-y-10 px-3">
         <div className="space-y-5 text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Developer Jobs
-          </h1>
+          <H1>Developer Jobs</H1>
           <p className="text-muted-foreground">Find Your Dream Job</p>
         </div>
         <section className="flex flex-col gap-4 md:flex-row">
--- src/components/JobFilterSideBar.tsx
+++ src/components/JobFilterSideBar.tsx
@@ -1,11 +1,37 @@
+import prisma from "@/lib/prisma";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import Select from "./ui/select";
+import { jobTypes } from "@/lib/job-types";
+import { Button } from "./ui/button";
+import { jobFilterSchema } from "@/lib/validation";
+import { redirect } from "next/navigation";
 
 async function filterJobs(formData: FormData) {
   "use server";
+  const values = Object.fromEntries(formData.entries());
+  const { q, type, location, remote } = jobFilterSchema.parse(values);
+
+  const searchParams = new URLSearchParams({
+    ...(q && { q: q.trim() }),
+    ...(type && { type }),
+    ...(location && { location }),
+    ...(remote && { remote: "true" }),
+  });
+  redirect(`/${searchParams.toString()}`);
 }
 
-export default function JobFilterSideBar() {
+export default async function JobFilterSideBar() {
+  const distnictLocations = (await prisma.job
+    .findMany({
+      where: { approved: true },
+      select: { location: true },
+      distinct: ["location"],
+    })
+    .then((locations) =>
+      locations.map(({ location }) => location).filter(Boolean),
+    )) as string[];
+
   return (
     <aside className="h-fit md:w-[260px] sticky top-0 bg-background border rounded-lg p-4">
       <form action={filterJobs}>
@@ -14,6 +40,39 @@ export default function JobFilterSideBar() {
             <Label htmlFor="q">Search</Label>
             <Input id="q" name="q" />
           </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="type">Type</Label>
+            <Select id="type" name="type" defaultValue="">
+              <option value="">All location</option>
+              {jobTypes.map((jobType) => (
+                <option key={jobType} value={jobType}>
+                  {jobType}
+                </option>
+              ))}
+            </Select>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="location">Location</Label>
+            <Select id="location" name="location" defaultValue="">
+              <option value="">All location</option>
+              {distnictLocations.map((location) => (
+                <option key={location} value={location}>
+                  {location}
+                </option>
+              ))}
+            </Select>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              id="remote"
+              type="checkbox"
+              name="remote"
+              className="scale-125 accent-black"
+            />
+            <Label htmlFor="remote">Remote</Label>
+          </div>
+
+          <Button type="submit">Filter Jobs</Button>
         </div>
       </form>
     </aside>
